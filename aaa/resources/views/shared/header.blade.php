ee7359200870372237c3eaa81a47aa55b80f4a17
student <student@ur.edu.pl>
2024-03-12 09:22:53 +0100

    test -1.0

--- aaa/resources/views/shared/header.blade.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>VODRental</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-</head>
